2200175d2e60cf0cecbb21a5e330a9bb778761c9
sancodes <munsan14@gmail.com>
2021-04-04 16:23:06 -0700

    add logic to store values in courseObj

--- components/Term/Inputs.js
+++ components/Term/Inputs.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, SafeAreaView, ScrollView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, Text } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import RNPickerSelect from 'react-native-picker-select';
 
-let course = new Array();
+let courseObj = {};
 
 export default function Input() {
 
@@ -25,16 +23,13 @@ export default function Input() {
     ];
     let [course, setCourse] = useState('');
     let [credit, setCredit] = useState('');
-    let [grade, setGrade] = useState(''); //ignore that grade, still being used. 
 
-    let setCreditArr = (c) => {
-        setCredit(c);
-    }
+    //setting the useState variables
     let setCourseArr = (c) => {
         setCourse(c);
     }
-    let setGradeArr = (v) => {
-        setGrade(v);
+    let setCreditArr = (c) => {
+        setCredit(c);
     }
     return (
         <View style={styles.termSection}>
@@ -42,32 +37,36 @@ export default function Input() {
                 <Text style={styles.inputSection}> Term # </Text>
             </View>
             <View style={styles.rowContainer}>
-                <Text style={styles.section}>Course </Text>
-                <Text style={styles.section}> Grade </Text>
-                <Text style={styles.section}> Credit </Text>
+                <Text style={styles.courseSection}> Course </Text>
+                <Text style={styles.gradeSection}> Grade </Text>
+                <Text style={styles.creditSection}> Credit </Text>
             </View >
 
             <View style={styles.rowContainer}>
                 <TextInput
-                    style={styles.section}
+                    style={styles.courseSection}
                     onChangeText={setCourseArr}
+                    onEndEditing={() => { courseObj.course = course }}
                     value={course}
                 />
-                <View style={styles.section}>
+                <View style={styles.gradeSection}>
                     <RNPickerSelect
-                        onValueChange={setGradeArr}
+                        onValueChange={(input) => { courseObj.grade = input }}
                         items={gradeScale}
                     />
                 </View>
                 <TextInput
-                    style={styles.section}
+                    style={styles.creditSection}
                     onChangeText={setCreditArr}
+                    onEndEditing={() => { courseObj.credit = credit }}
                     value={credit}
                 />
             </View>
         </View >
     );
 }
+
+
 
 const styles = StyleSheet.create({
 
@@ -81,19 +80,28 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    bodyRowContainer: {
-        marginTop: 34,
-        flexDirection: 'row',
-    },
     inputSection: {
         backgroundColor: 'lightgray',
         marginRight: 35,
         marginLeft: 15,
     },
-    section: {
+    gradeSection: {
         backgroundColor: 'lightblue',
         flex: 1,
 
+    },
+    courseSection: {
+        backgroundColor: 'pink',
+        flex: 2,
+    },
+    creditSection: {
+        backgroundColor: 'lightgreen',
+        flex: 1,
+
+    },
+    checkboxSection: {
+        backgroundColor: 'white',
+        flex: 1,
     }
 
 })
