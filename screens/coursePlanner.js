e11442afc8b0e3c4c0eb915231bc574267ec90bf
sancodes <munsan14@gmail.com>
2021-08-02 13:39:04 -0700

    initial commit

---
+++ screens/coursePlanner.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+export default function CoursePlanner({ navigation, route }) {
+    return (
+        <View>
+            <Text>
+                add calendar here
+            </Text>
+        </View>
+    );
+}
